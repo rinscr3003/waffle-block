e3f1dc039053ab38c575b41f546083b864e8102c
雨纷々 <rinscr3003@foxmail.com>
2021-07-24 12:38:28 +0800

    Update drv_tft.h

--- waffle_default/drv_tft.h
+++ waffle_default/drv_tft.h
@@ -243,5 +243,6 @@ void drv_TFT_setbacklight(bool s);
 void drv_TFT_drawtime60px(uint8_t y, uint8_t h, uint8_t m);
 void drv_TFT_loadfont();
 void drv_TFT_unloadfont();
+void drv_TFT_drawpng(uint8_t x, uint8_t y, String filepath);
 
 #endif
